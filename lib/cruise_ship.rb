c0b77396a87a18f497d486d932307fc85fc98536
Ward Price <wardprice@gmail.com>
2020-06-29 16:29:34 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,12 +7,12 @@
 # }
 
 def select_winner(passengers)
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
   end
-end
  
-winner
+  winner
 end
