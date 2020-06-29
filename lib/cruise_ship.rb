fa0ae8f5ac8e4aa5a8508c016e8ef56fb8b96290
Ward Price <wardprice@gmail.com>
2020-06-29 16:29:03 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,14 +6,6 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-
 def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
-  end
-end
 
-winner
+end
